406a5d328db72d21f6ca6153feac1f4b894dc89b
Enoch <idowuenoch65@gmail.com>
2023-08-04 22:09:47 +0000

    Working with more typing functions

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,5 +2,10 @@
 """Contains to_kv function"""
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple with passed string and square of int
+    passed
+    """
     return (k, (v * v))
