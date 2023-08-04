894ef4cd664a62bd37970cbcfe3b2ce625719548
Enoch <idowuenoch65@gmail.com>
2023-08-04 22:06:11 +0000

    More typing functions

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Contains to_kv function"""
+from typing import Tuple, Union
 
-
-def to_kv(k: str, v: int) -> tuple(str, float):
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     return (k, (v * v))
