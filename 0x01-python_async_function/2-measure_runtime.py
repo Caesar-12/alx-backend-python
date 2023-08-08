e0d7938c6fe7c6824109fce0a7df2d48c831be66
Enoch <idowuenoch65@gmail.com>
2023-08-08 03:47:42 +0000

    Calculating elasped time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Contains a measure_time function"""
+import time
+import asyncio
+
+
+def measure_time(n, max_delay):
+    """Calculates elapsed time fir wait_n function"""
+    wait_n = __import__('1-concurrent_coroutines').wait_n
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    time_taken = end_time - start_time
+
+    return time_taken
