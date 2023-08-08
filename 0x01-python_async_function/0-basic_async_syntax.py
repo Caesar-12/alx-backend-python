e7d6b1217113187a70793217a3af7ecf3023ec34
Enoch <idowuenoch65@gmail.com>
2023-08-08 03:30:43 +0000

    Synchronous functions

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay=10):
     """Simple coroutine function"""
     wait_time = random.uniform(0, max_delay)
     await asyncio.sleep(wait_time)
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,8 +7,10 @@ async def wait_n(n, max_delay):
     """Utilizes the wait_random function"""
     wait_random = __import__('0-basic_async_syntax').wait_random
     wait_list = []
-    wait_time = asyncio.create_task(wait_random(max_delay))
-    print(type(wait_time))
-    wait_list.append(wait_time)
+    for i in range(n):
+        wait = asyncio.create_task(wait_random(max_delay))
+        wait_time = await wait
+        # print(type(wait_time))
+        wait_list.append(wait_time)
 
     return wait_list
