a94e9ef44c88e95193f643e42ef7a64bb0fd9a5f
Enoch <idowuenoch65@gmail.com>
2023-08-09 00:51:13 +0000

    async.gather

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
-""""""
+"""Contains measure_runtime coroutine"""
+import asyncio
+import time
+
+
+async def measure_runtime() -> float:
+    """Returns the time takem to run async_comprehension"""
+    comp = __import__('1-async_comprehension').async_comprehension
+    start_time = time.time()
+    await asyncio.gather(
+        comp(),
+        comp(),
+        comp(),
+        comp()
+    )
+    end_time = time.time()
+    time_taken = end_time - start_time
+
+    return time_taken
